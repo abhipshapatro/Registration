76d9629c6f3b6731e059c2819b125dcd77867b38
abhipshapatro <71815946+abhipshapatro@users.noreply.github.com>
2022-12-24 22:32:51 +0530

    chore: deleted files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Screen1 from "./screens/Screen1";
 import Screen2 from "./screens/Screen2";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Screen3 from "./screens/Screen3";
-import PhoneNumberInput from "./components/PhoneNumberInput";
-
-
 
 function App() {
   return (
@@ -14,7 +11,6 @@ function App() {
           <Route path="/" element={<Screen1 />} />
           <Route path="/password" element={<Screen2 />} />
           <Route path="/modal" element={<Screen3 />} />
-          <Route path="/phone" element={<PhoneNumberInput />} />
         </Routes>
       </Router>
     </div>
